81bfd6b713f0638112835f731ae6d7125d4ba842
SLI0095 <simon.sliva.st@vsb.cz>
2018-12-05 13:36:01 +0100

    final version Signed-off-by: SLI0095 <simon.sliva.st@vsb.cz>

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	public static int WIDTH = 1920;
+	public static int HEIGHT = 1080;
+
+	Playground playground;
+
+	@Override
+	public void create () {
+		playground = new Playground();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		playground.handle_input();
+		playground.update(Gdx.graphics.getDeltaTime());
+		playground.render();
+	}
+
+	//@Override
+	/*public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}*/
+}
